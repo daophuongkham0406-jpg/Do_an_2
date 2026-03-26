4a0e578eb771d599ab25452e376b401d9a8b3da5
tridepza <nguyentri01022005@gmail.com>
2026-03-26 16:52:50 +0700

    qq

--- Fontend/function_proc/check_login.js
+++ Fontend/function_proc/check_login.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // Lấy fullName từ dữ liệu Database hiện lên
         userMenu.innerHTML = `
-            <span style="color: #c6ff00; font-weight: 600; font-size: 15px;">👋 Xin chào, ${user.fullName}</span>
+            <span style="color: #c6ff00; font-weight: 600; font-size: 15px;"> ${user.fullName}</span>
             <button id="logout-btn" style="background: transparent; border: 1px solid #fff; color: #fff; padding: 8px 16px; border-radius: 6px; cursor: pointer; transition: 0.3s;">
                 Đăng xuất
             </button>
